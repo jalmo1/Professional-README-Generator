3b2c86c4829ddf31ee103184fdba4e45c01b016c
Joshua Almodovar <joshuarosamktp4@gmail.com>
2021-06-25 15:57:27 -0400

    add gif

---
+++ Develop/README.md
@@ -0,0 +1,33 @@
+
+  
+  ### [![license](https://img.shields.io/badge/license-MozillaPublicLicense2.0-blueviolet)](https://shields.io)
+
+
+  ## Table of contents:
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributors](#contributors)
+  - [Contact](#contact)
+
+  # Project name!
+
+  ## Description:
+    Description goes here!
+  ## Video:
+
+  ## Test:
+  Put instructions on testing here!
+
+  ## Installation:
+  Install process
+
+  ## Usage:
+  To fix a problem
+
+  ## Contributors:
+  no
+
+  ## Contact:
+    - Email: josh@gmail.com
+    - Github: https://github.com/jalmo1
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,38 +1,3 @@
-/*
-Game plan=>
-
-LICENSE BADGE
-
-then TABLE OF CONTENTS
-
-then TITLE
-
-then DETAILED DESCRIPTION
-
-then VIDEO 
-
-then INSTALLATION
-
-then USAGE
-
-then CONTRIBUTORS 
-
-then CONTACT
-*/
-
-/*
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-*/
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
